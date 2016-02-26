dbd22831ad731f196df272455ec7fda8e5ac5a27
chenhuiyangxin <240695717@qq.com>
2016-02-26 22:34:31 +0800

    second

---
+++ connect.js
@@ -0,0 +1,14 @@
+var knex = require('knex')({
+    client: 'mysql',
+    connection: {
+        host     : '127.0.0.1',
+        user     : 'root',
+        password : '123456',
+        database : 'factory'
+    },
+    pool: {
+        min: 0,
+        max: 7
+    }
+});
+exports.db=knex;
