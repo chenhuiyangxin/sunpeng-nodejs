f1059038c191a563e884cd6d3cd2eaf30f54edb7
chenhuiyangxin <240695717@qq.com>
2016-02-27 23:40:17 +0800

    test

--- routes/detail.js
+++ routes/detail.js
@@ -1,6 +1,7 @@
 var db = require("../connect").db;
 module.exports = function(req,res){
-var id = req.params.id;
+/*var id = req.params.id;*/
+var id = db.select('id').from('plant').limit(1);
   db.select('*').from('plant').where('id',id).then(function(resp) {
         res.render("detail", {
           factorys:resp
